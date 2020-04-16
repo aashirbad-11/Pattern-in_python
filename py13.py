9510e1deecd5552c15b0b0e556ff687868ac04df
Aashirbad Maharana <aashirbadmaharana11@gmail.com>
2020-04-17 00:32:54 +0530

    day 3 commit

---
+++ py13.py
@@ -0,0 +1,12 @@
+for i in range(65, 70):
+    for j in range(65, i + 1):
+        print(str(chr(i)), ' ',end = '' )
+    print()
+
+
+    
+# A  
+# B  B  
+# C  C  C  
+# D  D  D  D  
+# E  E  E  E  E  
---
+++ py14.py
@@ -0,0 +1,10 @@
+for i in range(65, 70):
+    for j in range(65, i +1):
+        print(str(chr(j)),' ', end ="")
+    print()
+
+# A  
+# A  B  
+# A  B  C  
+# A  B  C  D  
+# A  B  C  D  E  
